e53d571e02f526efbaaff012d74a4885f358f2ff
Gabriel Roquim <gabrielroquim@hotmail.com>
2022-09-20 22:18:19 -0300

    sal vando

---
+++ cypress.config.js
@@ -0,0 +1,16 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    
+      baseUrl: 'http://lojaebac.ebaconline.art.br/',
+      reporter: 'mochawesome',
+      reporterOptions: {
+        reportDir: 'cypress/results',
+        overwrite: false,
+        html: false,
+        json: true
+      }
+   
+  }
+});
